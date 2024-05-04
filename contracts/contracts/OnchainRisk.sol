5927b8f8501067ca314a5ff5b354a7353ae90eab
Ethan Davis <ethantddavis@gmail.com>
2024-05-05 07:53:53 +1200

    made contract smart, still expensive

--- contracts/contracts/OnchainRisk.sol
+++ contracts/contracts/OnchainRisk.sol
@@ -7,53 +7,76 @@ struct GameState {
   address player0;
   address player1;
   address player2;
+  uint256 turnId;
   bool started;
 }
 
-contract OnchainRisk {
-  UltraVerifier public verifier;
-  constructor() {
-    verifier = new UltraVerifier();
-  }
+struct Board {
+  bytes32[42] owners;
+  bytes32[42] population;
+}
 
-  function verifyProof(bytes calldata _proof, bytes32[] calldata _publicInputs) external view returns (bool) {
-    return verifier.verify(_proof, _publicInputs);
-  }
+contract OnchainRisk {
+
+  UltraVerifier public verifier;
+
+  uint256 public nextGameId;
 
   event CreateGame(uint256 gameId, uint256 turnLength);
   event JoinGame(uint256 gameId, address player);
   event TurnStart(uint256 gameId, address player);
-
-  uint256 nextGameId = 0;
+  
   mapping(uint256 => GameState) gameState;
-  mapping(uint256 => uint8) boards;
+  mapping(uint256 => Board) boards;
 
-  function createGame(uint256 turnLength /*, GameBoard calldata startingBoard */) external {
+  constructor() {
+    verifier = new UltraVerifier();
+
+  }
+
+  function createGame(uint256 turnLength, bytes32[42] calldata owners, bytes32[42] calldata population) external {
     uint256 id = nextGameId;
 
     emit CreateGame(id, turnLength);
     emit JoinGame(id, msg.sender);
+
     nextGameId++;
+
   }
 
   function joinGame(uint256 id) external {
     require(gameState[id].player0 != address(0), "Game does not exist");
     require(gameState[id].started == false, "Game already started");
+
     emit JoinGame(id, msg.sender);
+
     if (gameState[id].player1 == address(0)) {
       gameState[id].player1 = msg.sender;
+
     } else if (gameState[id].player2 == address(0)) {
+
       gameState[id].player2 = msg.sender;
       gameState[id].started = true;
+
       emit TurnStart(id, gameState[id].player0);
+
     }
   }
 
-//  function completeTurn(GameBoard calldata newBoard, Actions calldata actions, bytes32 seed) {
-    // arre you the current turn address
-  //
-    // make zk verification
-//    v    // update board
- // }
+  function completeTurn(uint256 gameId, bytes calldata _proof, bytes32[] calldata _publicInputs) external {
+    require(verifier.verify(_proof, _publicInputs), "Invalid Turn");
 
+    for (uint256 i; i < 84; i++) { // tile owners + tile pop
+      
+      if (i < 42) {
+        boards[gameId].owners[i] = _publicInputs[i];
+        
+      } else {
+        boards[gameId].population[i - 42] = _publicInputs[i];
+      } 
+    }
+
+    gameState[gameId].turnId = uint256(_publicInputs[87]);
+
+  }
 }
